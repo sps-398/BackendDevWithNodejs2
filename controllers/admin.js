8811ce4792cfd49aa5b31f3447e6ca4af0c263c9
sps-398 <shashankpratapsingh5004@gmail.com>
2023-04-12 05:46:14 +0530

    Nodejs:Ass2-task10-Add edit and delete functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   });
 };
 
@@ -15,10 +13,48 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = Number(req.body.price);
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  Product.findById(req.params.productId, (product) => {
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  })
+};
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedPrice = Number(req.body.price);
+    const updatedDescription = req.body.description;
+
+    const updatedProduct = new Product(
+      prodId,
+      updatedTitle,
+      updatedImageUrl,
+      updatedDescription,
+      updatedPrice
+    );
+
+    updatedProduct.save();
+
+    res.redirect('/admin/products');
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+  Product.deleteById(req.params.productId, () => {
+    res.redirect('/admin/products');
+  });
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -27,11 +28,21 @@ module.exports = class Product {
 
   save() {
     getProductsFromFile(products => {
+      if(this.id) {
+        const existingProductIndex = products.findIndex(p => p.id === this.id);
+        let updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        })
+      }
+      else {
         this.id = Math.random().toString();
         products.push(this);
         fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
+      }
     });
   }
 
@@ -43,6 +54,17 @@ module.exports = class Product {
     getProductsFromFile((products) => {
         const product = products.find(p => p.id === prodId);
         cb(product);
+    });
+  }
+
+  static deleteById(prodId, cb) {
+    getProductsFromFile((products) => {
+      const productIndex = products.findIndex(p => p.id === prodId);
+      products.splice(productIndex, 1);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+      cb();
     });
   }
 };
