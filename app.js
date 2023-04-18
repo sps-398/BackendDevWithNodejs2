1b007753d6a3d61754af22f0e34a6608eb142afa
sps398 <vispsen@gmail.com>
2023-04-18 13:38:05 +0530

    Nodejs:Ass2-task21

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const sequelize = require('./util/database');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const errorController = require('./controllers/error');
 const db = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,36 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user=user;
+        next();
+    })
+    .catch(err => console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
     .sync()
     .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if(!user) {
+            return User.create({ name: 'Max', email: 'test@gmail.com' });
+        }
+        return user;
+    })
+    .then(user => {
+        console.log(user);
         app.listen(3000);
     })
     .catch(err => console.log(err));
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = Number(req.body.price);
   const description = req.body.description;
-  Product.create({
+  req.user
+  .createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
@@ -28,8 +29,9 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  Product.findByPk(req.params.productId)
-  .then(product => {
+  req.user.getProducts({ where: { id: req.params.productId }})
+  .then(products => {
+    const product = products[0];
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
@@ -48,9 +50,9 @@ exports.postEditProduct = (req, res, next) => {
 
     Product.findByPk(prodId)
       .then(product => {
-        product.title = updatedTitle,
-        product.price = updatedPrice,
-        product.imageUrl = updatedImageUrl,
+        product.title = updatedTitle;
+        product.price = updatedPrice;
+        product.imageUrl = updatedImageUrl;
         product.description = updatedDescription
 
         return product.save();
@@ -74,7 +76,8 @@ exports.postDeleteProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then((products) => {
+  req.user.getProducts()
+  .then((products) => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
